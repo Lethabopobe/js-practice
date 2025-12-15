7a4a6a52eb9f2c74344b8c495d467d32eeee4dcc
Lethabo Mokoape <101903974+Lethabopobe@users.noreply.github.com>
2025-12-16 01:58:42 +0200

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,58 @@
+document.getElementById("btn1").addEventListener("click",function
+(){
+document.getElementById("title").textContent= "Hello Lethabo Mokoape!" ;
+document.getElementById("title").style.font="30px";
+}
+);
+document.getElementById("btn2").addEventListener("click",function(){
+document.querySelector("body").style.backgroundColor="blue";
+
+});
+document.getElementById("btn3").addEventListener("click",function(){
+if (document.getElementById("info").style.display==="none"){
+    document.getElementById("info").style.display="block"
+     document.getElementById("info").textContent="This is some information."
+}
+else{ 
+document.getElementById("info").style.display="none"
+}}
+);
+document.getElementById("btn4").addEventListener("click",function(){
+document.querySelectorAll(".heading").forEach(heading=>{  
+  heading.style.color="green"  
+})
+})
+let counter=0;
+document.getElementById("btn5").addEventListener("click",function(){
+    counter=counter+1
+document.getElementById("count").textContent=counter
+})
+document.getElementById("btn6").addEventListener("click",function(){
+     counter=counter-1
+document.getElementById("count").textContent=counter
+})
+let scorecount=0;
+document.getElementById("right").addEventListener("click",function(){
+    document.getElementById("result").textContent="correct"
+   document.getElementById("result").style.color="green"
+   scorecount=scorecount+1
+   if(scorecount>1){
+    scorecount=scorecount-1
+  }
+  document.getElementById("score").textContent=scorecount
+  
+}
+)
+document.getElementById("wrong").addEventListener("click",function(){
+document.getElementById("result").textContent="incorrect"
+document.getElementById("result").style.color="red"
+document.getElementById("result").style.color="red"
+scorecount=scorecount-1
+document.getElementById("score").textContent=scorecount
+})
+document.getElementById("wrong2").addEventListener("click",function(){
+document.getElementById("result").textContent="incorrect"
+document.getElementById("result").style.color="red"
+scorecount=scorecount-1
+document.getElementById("score").textContent=scorecount
+})
